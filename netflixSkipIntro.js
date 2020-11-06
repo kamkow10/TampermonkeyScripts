f7641cf371e5ce4891b3834e69ea73ab2df46991
Kamil Kowalczyk <49336454+kamkow10@users.noreply.github.com>
2020-11-06 13:01:18 +0100

    add netflix skip intro script

---
+++ netflixSkipIntro.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         NetflixSkipIntro
+// @namespace    http://netflix.com/
+// @version      0.2
+// @updateURL    https://github.com/kamkow10/TampermonkeyScripts/netflixSkipIntro.user.js
+// @downloadURL  https://github.com/kamkow10/TampermonkeyScripts/netflixSkipIntro.user.js
+// @description  Netflix skip intro on space
+// @author       Kamil Kowalczyk
+// @match        https://www.netflix.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.addEventListener('keydown', (event) => {
+        const skipButton = document.getElementsByClassName('skip-credits')[0];
+        if (((event.key === ' ') || (event.key === 'Spacebar')) && (skipButton != null)) {
+            event.preventDefault();
+            skipButton.childNodes[0].click();
+            setTimeout(() => { document.getElementsByClassName('button-nfplayerPlay')[0].click(); }, 500);
+        }
+    });
+})();
