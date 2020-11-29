caac30f7d69838cf058ba314ee0760d0e6f637e8
Kamil Kowalczyk <49336454+kamkow10@users.noreply.github.com>
2020-11-29 20:51:26 +0100

    Create youtubeAutofocus.user.js

---
+++ youtubeAutofocus.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         YoutubeAutofocus
+// @namespace    https://github.com/kamkow10/
+// @version      0.1
+// @updateURL    https://github.com/kamkow10/TampermonkeyScripts/raw/main/youtubeAutofocus.user.js
+// @downloadURL  https://github.com/kamkow10/TampermonkeyScripts/raw/main/youtubeAutofocus.user.js
+// @description  Autofocus search input on YouTube site
+// @author       Kamil Kowalczyk
+// @match        https://www.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // code
+})();
