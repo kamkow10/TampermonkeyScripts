ae7c67d92a9d80bce76e7c779e6295fb39be761a
Kamil Kowalczyk <49336454+kamkow10@users.noreply.github.com>
2021-09-20 15:58:29 +0200

    Update siteReloader.user.js

--- siteReloader.user.js
+++ siteReloader.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         SiteReloader
 // @namespace    http://netflix.com/
-// @version      0.3
+// @version      0.4
 // @updateURL    https://github.com/kamkow10/TampermonkeyScripts/raw/main/siteReloader.user.js
 // @downloadURL  https://github.com/kamkow10/TampermonkeyScripts/raw/main/siteReloader.user.js
 // @description  Web site auto-reloader
 // @author       Kamil Kowalczyk
-// @match        *
+// @match        https://paniswojegoczasu.pl/sklep/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    setTimeout(window.location.reload(true), 10000);
+    setTimeout(() => window.location.reload(true), 10000);
 })();
