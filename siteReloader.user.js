aa81e849dde93ea791f981244ee630a4f9ef7fff
Kamil Kowalczyk <49336454+kamkow10@users.noreply.github.com>
2021-09-20 15:48:49 +0200

    Update siteReloader.user.js

--- siteReloader.user.js
+++ siteReloader.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         SiteReloader
 // @namespace    http://netflix.com/
-// @version      0.2
+// @version      0.3
 // @updateURL    https://github.com/kamkow10/TampermonkeyScripts/raw/main/siteReloader.user.js
 // @downloadURL  https://github.com/kamkow10/TampermonkeyScripts/raw/main/siteReloader.user.js
 // @description  Web site auto-reloader
 // @author       Kamil Kowalczyk
+// @match        *
 // @grant        none
 // ==/UserScript==
 
