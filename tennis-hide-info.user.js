29cf3ecb9c632925d7599f734d5904cf0d0ba7ff
Kamil Kowalczyk <49336454+kamkow10@users.noreply.github.com>
2023-01-21 22:13:05 +0100

    Update tennis-hide-info.user.js

--- tennis-hide-info.user.js
+++ tennis-hide-info.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Tennis Hide Info
-// @namespace    http://player.pl/
+// @namespace    https://player.pl/
 // @version      0.2
 // @updateURL    https://github.com/kamkow10/TampermonkeyScripts/raw/main/tennis-hide-info.user.js
 // @downloadURL  https://github.com/kamkow10/TampermonkeyScripts/raw/main/tennis-hide-info.user.js
 // @description  Script to hide match time
 // @author       Kamil Kowalczyk
-// @match        http://player.pl/eurosport*
+// @match        https://player.pl/eurosport*
 // @grant        none
 // ==/UserScript==
 
